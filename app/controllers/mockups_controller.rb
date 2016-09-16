fbccc6dfd3ca7d175b02aca42efd747ffbfddcce
Garrett Martin <me@garrettqmartin.com>
2016-09-16 11:52:56 -0500

    WIP dashboard.

--- app/controllers/mockups_controller.rb
+++ app/controllers/mockups_controller.rb
@@ -2,4 +2,6 @@ class MockupsController < ApplicationController
   def today; end
   def tomorrow; end
   def reflect; end
+  def nice_job; end
+  def dashboard; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get '/mockups/today' => 'mockups#today'
   get '/mockups/tomorrow' => 'mockups#tomorrow'
   get '/mockups/reflect' => 'mockups#reflect'
+  get '/mockups/nice_job' => 'mockups#nice_job'
+  get '/mockups/dashboard' => 'mockups#dashboard'
 end
