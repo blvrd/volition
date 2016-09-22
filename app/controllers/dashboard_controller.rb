fc2fd042d43116edf0fe4c7c97593bd3ccd2dd51
Garrett Martin <me@garrettqmartin.com>
2016-09-22 17:49:14 -0500

    Add eager loading for dashboard todos.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,8 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
+
   def show
-    @todays_todo_list = TodoList.today(current_user)
-    @past_todo_lists = TodoList.past(current_user)
+    @todays_todo_list = TodoList.includes(:todos).today(current_user)
+    @past_todo_lists = TodoList.includes(:todos).past(current_user)
   end
 end
