7a7dd72f12787f23894fd6f9e7d7d5f75a57b769
Garrett Martin <me@garrettqmartin.com>
2016-09-28 17:47:07 -0500

    Add puts statement.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,7 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def show
+    puts Time.zone
     @todays_todo_list = TodoList.includes(:todos).today(current_user)
     @past_todo_lists = TodoList.includes(:todos).past(current_user)
   end
