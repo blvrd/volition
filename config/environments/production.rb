c2e6b20259d8016f6e81a6dd5f8de45de3de79cd
Garrett Martin <me@garrettqmartin.com>
2016-10-04 14:03:14 -0500

    Set assets to compile.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.assets.js_compressor = :uglifier
-  config.assets.compile = false
+  config.assets.compile = true
   config.action_controller.asset_host = ENV.fetch("ASSET_HOST", ENV["APPLICATION_HOST"])
   config.log_level = :debug
   config.log_tags = [ :request_id ]
