2618b2903cb0cc3eda86e8435af664fb6b9c7628
Garrett Martin <me@garrettqmartin.com>
2016-11-23 22:39:32 -0500

    Fix before action.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, except: :new
+  before_action :authenticate_user!, except: [:new, :create]
 
   def new
     if current_user.present?
