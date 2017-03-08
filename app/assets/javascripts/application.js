01e362f131c535a29ab0a7e2f7d7194114cee7b8
Garrett Martin <me@garrettqmartin.com>
2017-03-08 10:26:16 -0600

    Add aload.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,7 +20,11 @@
 
 var update = React.addons.update
 
+function aload(t){"use strict";var e="data-aload";return t=t||window.document.querySelectorAll("["+e+"]"),void 0===t.length&&(t=[t]),[].forEach.call(t,function(t){t["LINK"!==t.tagName?"src":"href"]=t.getAttribute(e),t.removeAttribute(e)}),t}
+
 $(document).on('turbolinks:load', function() {
+  aload()
+
   var timezone = jstz.determine()
   cookies({timezone: timezone.name()})
 
