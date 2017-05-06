ac8942a412615202f44bcd163ae437d21fe66b3b
Garrett Martin <me@garrettqmartin.com>
2017-05-06 13:54:45 -0500

    Add copy and path changes based on guest status.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,10 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def show
+    if current_user.guest?
+      redirect_to today_path
+    end
+
     @tomorrows_todo_list = TodoList.includes(:todos).tomorrow(current_user)
     @todays_todo_list = TodoList.includes(:todos).today(current_user)
     @past_todo_lists = TodoList.includes(:todos).past(current_user).paginate(page: params[:page])
