ffe1715c6f0e3e516383cde406ab55af5834e4f3
Garrett Martin <me@garrettqmartin.com>
2017-05-20 18:21:45 -0500

    Add provider list.

--- app/controllers/marketing_controller.rb
+++ app/controllers/marketing_controller.rb
@@ -7,6 +7,54 @@ class MarketingController < ApplicationController
 
   def running_costs
     first_month = Date.new(2016, 10)
+    @list_data = [
+      {
+        name: 'Heroku',
+        color: 'rgba(150, 118, 185, 0.9)',
+        description: 'Platform as a service. The Volition app is hosted on Heroku.',
+        url: 'https://www.heroku.com/',
+        data: [
+          { x: first_month, y: 7 },
+          { x: first_month.advance({ months: 1 }), y: 9.38 },
+          { x: first_month.advance({ months: 2 }), y: 14.02 },
+          { x: first_month.advance({ months: 3 }), y: 14.02 },
+          { x: first_month.advance({ months: 4 }), y: 14.02 },
+          { x: first_month.advance({ months: 5 }), y: 14.02 },
+          { x: first_month.advance({ months: 6 }), y: 14.02 }
+        ]
+      },
+      {
+        name: 'Appsignal',
+        color: 'rgba(225, 100, 41, 0.9)',
+        description: 'Error and bug reporting.',
+        url: 'https://appsignal.com/',
+        data: [
+          { x: first_month, y: 0 },
+          { x: first_month.advance({ months: 1 }), y: 3.72 },
+          { x: first_month.advance({ months: 2 }), y: 12 },
+          { x: first_month.advance({ months: 3 }), y: 12 },
+          { x: first_month.advance({ months: 4 }), y: 12 },
+          { x: first_month.advance({ months: 5 }), y: 12 },
+          { x: first_month.advance({ months: 6 }), y: 12 },
+        ]
+      },
+      {
+        name: 'ExpeditedSSL',
+        color: 'rgba(37, 39, 43, 0.8)',
+        description: 'SSL certificate provider.',
+        url: 'https://www.expeditedssl.com/',
+        data: [
+          { x: first_month, y: 0 },
+          { x: first_month.advance({ months: 1 }), y: 0 },
+          { x: first_month.advance({ months: 2 }), y: 0 },
+          { x: first_month.advance({ months: 3 }), y: 0 },
+          { x: first_month.advance({ months: 4 }), y: 0 },
+          { x: first_month.advance({ months: 5 }), y: 13.18 },
+          { x: first_month.advance({ months: 6 }), y: 15 },
+        ]
+      }
+    ].map { |item| OpenStruct.new(item) }
+
     gon.chartData = [{
       label: 'Heroku',
       lineTension: 0,
