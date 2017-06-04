5714541eadc7fe37404e57e91e29461727560980
Garrett Martin <me@garrettqmartin.com>
2017-06-04 16:22:08 -0500

    Add test env conditional for redis.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,4 @@
-uri = URI.parse(ENV['REDISTOGO_URL'])
-$redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+unless Rails.env.test?
+  uri = URI.parse(ENV['REDISTOGO_URL'])
+  $redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+end
