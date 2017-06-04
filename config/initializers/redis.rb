9ed5f9a6ca7a106652de0fd601f26e92efec48fa
Garrett Martin <me@garrettqmartin.com>
2017-06-04 16:25:03 -0500

    Add else condition.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,6 @@
-unless Rails.env.test?
+if Rails.env.test?
+  $redis = Redis.new
+else
   uri = URI.parse(ENV['REDISTOGO_URL'])
   $redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
 end
