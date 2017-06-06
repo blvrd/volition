9f4433abb1e38ddccea9608c6ec64e05f617fa4c
Garrett Martin <me@garrettqmartin.com>
2017-06-06 15:06:07 -0500

    Add test for guest user cleanup job.

--- app/jobs/destroy_guest_users_job.rb
+++ app/jobs/destroy_guest_users_job.rb
@@ -2,7 +2,7 @@ class DestroyGuestUsersJob < ApplicationJob
   queue_as :destroy_guest_users
 
   def perform
-    User.where(guest: true).each do |user|
+    User.where('guest = true and created_at < ?', 1.hour.ago).each do |user|
       user.destroy
     end
   end
