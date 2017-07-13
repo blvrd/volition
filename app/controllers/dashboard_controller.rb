85c728c8083d6d4b162fb8faedf21e3fe21a3db3
Garrett Martin <me@garrettqmartin.com>
2017-07-13 11:32:28 -0500

    Add 'new' class to new weekly todo to highlight it.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,6 +6,6 @@ class DashboardController < AuthenticatedController
 
     @tomorrows_todo_list = TodoList.includes(:todos).tomorrow(current_user)
     @todays_todo_list = TodoList.includes(:todos).today(current_user)
-    @past_todo_lists = TodoList.includes(:todos).past(current_user).paginate(page: params[:page])
+    @past_todo_lists = TodoList.includes(:todos).past(current_user).daily.paginate(page: params[:page])
   end
 end
