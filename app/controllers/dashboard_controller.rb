3ccbfe3bfe04d19545c9aede8b3a066200bfb330
Garrett Martin <me@garrettqmartin.com>
2017-07-17 16:02:30 -0500

    Add week header.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,7 +4,10 @@ class DashboardController < AuthenticatedController
       redirect_to today_path
     end
 
-    @tomorrows_todo_list = TodoList.includes(:todos).tomorrow(current_user)
+    unless params[:page]
+      @tomorrows_todo_list = TodoList.includes(:todos).tomorrow(current_user)
+    end
+
     @todays_todo_list = TodoList.includes(:todos).today(current_user)
     @past_todo_lists = TodoList.includes(:todos).past(current_user).daily.paginate(page: params[:page])
   end
