b40c5947db2685ac8896477c3afa6fda52a6d3b9
Garrett Martin <me@garrettqmartin.com>
2017-09-06 17:53:19 -0500

    Add check for env variable.
    Because:
    
    The deploy to Heroku button is launching builds that break due to a
    missing env variable.
    
    This:
    
    Adds a guard clause to short circuit before redis attempts to use the
    env variable.

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,8 @@
 if Rails.env.test?
   $redis = Redis.new
 else
+  return unless ENV['REDISTOGO_URL']
+
   uri = URI.parse(ENV['REDISTOGO_URL'])
   $redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
 end
