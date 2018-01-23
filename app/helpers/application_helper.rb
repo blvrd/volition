f08b6d1f7ce10beee24e0aa3c1f0eb898a6b2e6a
Garrett Martin <me@garrettqmartin.com>
2018-01-23 12:55:14 -0600

    Use proper truncation to fix mobile list style.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,14 +23,6 @@ module ApplicationHelper
     end
   end
 
-  def truncate(string, user_agent: :desktop)
-    if user_agent == :desktop
-      string.truncate(40)
-    else
-      string.truncate(25)
-    end
-  end
-
   def self_hosted?
     ENV['SELF_HOSTED'] == 'true'
   end
