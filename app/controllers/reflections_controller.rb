a5672b475f55bb9fae9e447a3d9cd30f05548409
Garrett Martin <me@garrettqmartin.com>
2018-01-26 11:26:20 -0600

    Allow users to reflect on the previous day.

--- app/controllers/reflections_controller.rb
+++ app/controllers/reflections_controller.rb
@@ -2,26 +2,38 @@ class ReflectionsController < AuthenticatedController
   include ApplicationHelper
 
   def new
-    if Reflection.today(current_user).present?
+    @reflection = Reflection.new
+    @todo_list = TodoList.find_by(date: params[:date]) ||
+      TodoList.today(current_user)
+    @daily_snapshot = @todo_list.daily_snapshot
+    @reflecting_on_today = @todo_list == TodoList.today(current_user)
+    @link_text, @link_url = if @reflecting_on_today
+                              ["Go back to today's tasks", today_path]
+                            else
+                              ["Go back", day_path(@daily_snapshot)]
+                            end
+
+    if Reflection.today(current_user).present? && @reflecting_on_today
       flash[:error] = 'You already wrote your reflection for today.'
       redirect_to dashboard_path
-    elsif TodoList.today(current_user).blank?
+    elsif params[:todo_list_id].blank? && TodoList.today(current_user).blank?
       flash[:error] = 'You must start your tasks for today before writing a reflection.'
       redirect_to dashboard_path
     end
-
-    @reflection = Reflection.new
-    @todo_list = TodoList.today(current_user)
   end
 
   def create
     @reflection = Reflection.new(reflection_params)
     @reflection.user = current_user
-    @reflection.date = Date.current
 
-    DailySnapshot.create_from_todo_list(TodoList.today(current_user))
+    @todo_list = TodoList.find_by(date: reflection_params[:date])
+    @daily_snapshot = @todo_list.daily_snapshot
 
     if @reflection.save
+      if @daily_snapshot.blank?
+        @daily_snapshot = DailySnapshot.create_from_todo_list(@todo_list)
+      end
+
       Todo.where(id: params[:add_to_week]).each do |todo|
         todo.update(
           weekly_todo_list_id: current_week_plan.id,
@@ -31,7 +43,6 @@ class ReflectionsController < AuthenticatedController
 
       redirect_to after_create_path
     else
-      @todo_list = TodoList.today(current_user)
       render :new
     end
   end
@@ -39,7 +50,9 @@ class ReflectionsController < AuthenticatedController
   private
 
   def after_create_path
-    if tomorrow_is_trackable?
+    if @reflection.date != Date.current
+      day_path(@daily_snapshot)
+    elsif tomorrow_is_trackable?
       tomorrow_path
     else
       flash[:success] = 'Nice job today! Get some rest.'
@@ -48,6 +61,6 @@ class ReflectionsController < AuthenticatedController
   end
 
   def reflection_params
-    params.require(:reflection).permit(:rating, :positive, :negative, :undone)
+    params.require(:reflection).permit(:rating, :positive, :negative, :undone, :date)
   end
 end
