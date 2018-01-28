26553c7815a48e4f0237818fbdeeea3af45817f7
Garrett Martin <me@garrettqmartin.com>
2018-01-27 18:49:07 -0600

    Add rake task for creating daily snapshots every night.

--- app/jobs/create_daily_snapshots_job.rb
+++ app/jobs/create_daily_snapshots_job.rb
@@ -2,7 +2,8 @@ class CreateDailySnapshotsJob < ApplicationJob
   queue_as :daily_snapshots
 
   def perform
-    TodoList.missing_snapshot.find_each do |todo_list|
+    TodoList.where(user_id: User.finishing_their_day.pluck(:id))
+            .missing_snapshot.find_each do |todo_list|
       DailySnapshot.create_from_todo_list(todo_list)
     end
   end
